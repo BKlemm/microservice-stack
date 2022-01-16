a6d237992f8aa22ba2209ac996e26f01e2610027
BKlemm <bkl3mm@googlemail.com>
2022-01-16 13:06:39 +0100

    [MSS] #extend xstream security config

--- services/carpark-command/src/main/java/com/avondock/carparkcommand/CarparkCommandApplication.java
+++ services/carpark-command/src/main/java/com/avondock/carparkcommand/CarparkCommandApplication.java
@@ -1,5 +1,7 @@
 package com.avondock.carparkcommand;
 
+import com.thoughtworks.xstream.XStream;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,11 @@ public class CarparkCommandApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CarparkCommandApplication.class, args);
+    }
+
+    @Autowired
+    public void configureXStreamSecurity(XStream xStream) {
+        xStream.allowTypesByWildcard(new String[]{"com.avondock.carpark.cqrs.coreapi.**"});
     }
 
 }
--- services/carpark-query/src/main/java/com/avondock/carparkquery/CarparkQueryApplication.java
+++ services/carpark-query/src/main/java/com/avondock/carparkquery/CarparkQueryApplication.java
@@ -1,5 +1,7 @@
 package com.avondock.carparkquery;
 
+import com.thoughtworks.xstream.XStream;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,11 @@ public class CarparkQueryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarparkQueryApplication.class, args);
+	}
+
+	@Autowired
+	public void configureXStreamSecurity(XStream xStream) {
+		xStream.allowTypesByWildcard(new String[]{"com.avondock.carpark.cqrs.coreapi.**"});
 	}
 
 }
